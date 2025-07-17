bed3fcff1e2f4b42198a7678c7dcd519085713cd
CEC <learnmorececc1@gmail.com>
2025-07-17 12:38:42 +0200

    Kommetare hinzugefügt

--- Wohnung.java
+++ Wohnung.java
@@ -1,10 +1,14 @@
 public class Wohnung
 {
-    private String ort;
-    private boolean balkon;
-    private int preis;
-    private int anzZimmer;
+    private String ort;  // Wo der Wohnung sich befindet.
+    private boolean balkon; //Ob die Wohnung Balkon hat (Ja oder Nein).
+    private int preis;      // Wie viel der Wohnung kostet.
+    private int anzZimmer; // Wie viele Zimmer der Wohnung hat.
     
+    /*
+     * Constructor
+     * It is creating a new Instance of the class Wohnung.
+     */
     public Wohnung(String newOrt, boolean newBalkon, int newPreis, int newAnzZimmer)
     {
         setOrt(newOrt);
@@ -15,21 +19,25 @@ public class Wohnung
     
     public void setOrt(String newOrt)
     {
+        //Die Eigenschaft ort wird auf den Wert den übergebenen Parameters newOrt gesetzt.
         ort = newOrt;
     }
     
     public void setBalkon(boolean newBalkon)
     {
+        // Die Eigenschaft balkon wird überschrieben mit dem Wert des Parameters (newBalkon).
         balkon = newBalkon;
     }
     
     public void setPreis(int newPreis)
     {
+        // Die Eigenschaft preis wird auf den Wert den übergebenen Parameters gesetzt.
         preis = newPreis;
     }
     
     public void setAnzZimmer(int newAnzZimmer)
     {
+        // Die Eigenschaft anzZimmer wird auf den Wert den übergebenen Parameter gesetzt.
         anzZimmer = newAnzZimmer;
     }
     
